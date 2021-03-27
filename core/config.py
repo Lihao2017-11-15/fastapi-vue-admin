812be0f02a6b276c8d565486853a7ec9ae2e3c92
Lihao2017-11-15 <812256@qq.com>
2021-03-27 17:22:28 +0800

    修改wsl中的数据库路径

--- core/config.py
+++ core/config.py
@@ -9,23 +9,24 @@ class Settings:
     # api前缀
     API_PREFIX = "/api"
     # jwt密钥,建议随机生成一个
-    SECRET_KEY = "ShsUP9qIP2Xui2GpXRY6y74v2JSVS0Q2YOXJ22VjwkI"
+    SECRET_KEY = "ShsUP9qIP2Xui2GpXRY6y_4v2JSVS0Q2YOXJ22VjwkI"
     # token过期时间
     ACCESS_TOKEN_EXPIRE_MINUTES = 24 * 60
     # 跨域白名单
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:9528"]
     # db配置
-    DB_URL = "mysql+pymysql://root:Aa123456@127.0.0.1:3306/fast"
+    DB_URL = "mysql+pymysql://fastapi_vue:YmmPsJwxEJFZzpFW@localhost:3306/fastapi_vue"
+    
     # 启动端口配置
     PORT = 8999
     # 是否热加载
     RELOAD = True
     # 上传文件存储位置
-    UPLOAD_FOLDER = "D:\\code\\upload_files"
+    UPLOAD_FOLDER = "./upload_files"
     if not os.path.exists(UPLOAD_FOLDER):
         os.mkdir(UPLOAD_FOLDER)
     # CMDB模板文件存储位置
-    CMDB_FOLDER = "D:\\code\\cmdb_files"
+    CMDB_FOLDER = "./cmdb_files"
     if not os.path.exists(CMDB_FOLDER):
         os.mkdir(CMDB_FOLDER)
 
